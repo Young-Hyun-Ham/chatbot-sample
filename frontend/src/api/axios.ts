dfa6f74890a073650671ed8148807f9a0de89a56
함영현 <hyh8414@gmail.com>
2025-08-12 15:57:24 +0900

    backend 주소 변경

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -4,7 +4,7 @@ import {type CreateAxiosDefaults} from 'axios';
 import { SESSION_TIME } from '../config/session';
 
 const api = axios.create({
-  baseURL: 'https://chatbot-backend-ruddy-iota.vercel.app/api', // 백엔드 주소
+  baseURL: 'https://chatbot-sample-azure.vercel.app/api', // 백엔드 주소
   headers: {
     'Content-Type': 'application/json',
   },
