9003cd5097397b62280b7fa20d51d300bbb93c0f
함영현 <hyh8414@gmail.com>
2025-08-12 16:17:23 +0900

    backend url https 추가

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -4,7 +4,7 @@ import {type CreateAxiosDefaults} from 'axios';
 import { SESSION_TIME } from '../config/session';
 
 const api = axios.create({
-  baseURL: 'chatbot-sample-git-main-younghyunhams-projects.vercel.app/api', // 백엔드 주소
+  baseURL: 'https://chatbot-sample-git-main-younghyunhams-projects.vercel.app/api', // 백엔드 주소
   headers: {
     'Content-Type': 'application/json',
   },
